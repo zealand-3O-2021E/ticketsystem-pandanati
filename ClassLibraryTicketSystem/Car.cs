9e0956ac7eba266b704b7bbf61a3707b39fb9005
pandanati <72080264+pandanati@users.noreply.github.com>
2021-09-28 13:59:56 +0200

    Task 1 and 2

---
+++ ClassLibraryTicketSystem/Car.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryTicketSystem
+{
+    public class Car
+    {
+        #region Instance Fields
+        private string _licensePlate;
+        private DateTime _date;
+        #endregion
+
+        #region Properties
+        public string LicensePlate
+        {
+            get { return _licensePlate; }
+            set { _licensePlate = value; }
+        }
+
+        public DateTime Date
+        {
+            get { return _date; }
+            set { _date = value; }
+        }
+        #endregion
+
+        /// <summary>
+        /// Method which returns the price 240
+        /// </summary>
+        /// <returns>240</returns>
+        #region Methods
+        public double Price()
+        {
+            return 240;
+        }
+        
+        /// <summary>
+        /// Method which retusn the vehicle type "Car"
+        /// </summary>
+        /// <returns>Car</returns>
+        public string VehicleType()
+        {
+            return "Car";
+        }
+        #endregion
+    }
+}
